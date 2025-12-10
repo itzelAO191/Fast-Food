73a525971d6b343e35a2dd417cb99afb1e97074c
jcvergara1005-hash <j.c.vergara.1005@gmail.com>
2025-12-09 19:44:41 -0600

    se arreglo la pantalla pedido

--- fastfood/settings.py
+++ fastfood/settings.py
@@ -77,12 +77,12 @@ WSGI_APPLICATION = 'fastfood.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE':"django.db.backends.mysql",
-        'NAME': os.getenv("DB_Name"),
-        'USER': os.getenv("DB_User"),
+        'ENGINE': "django.db.backends.mysql",
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
         'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_Host"),
-        'PORT': os.getenv("DB_Port"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT"),
     }
 }
 
